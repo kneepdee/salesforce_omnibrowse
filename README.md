cf401ab1fd0e7c340d11a89af47d289b10f6c766
paniagua <carlos.paniagua@salemove.com>
2017-06-05 14:07:58 +0300

    Add OmniBrowse V1.7 app to source control
    OB-150

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # salesforce_omnibrowse
 Keeps SalesForce OmniBrowse App files
+
+There is no clear standard architecture to keep the source code for a visualsource page in here. 
+
+#TODO
+
+- Add instructions to setup
+- Add more test cases
+- Move JS out of this app
+- Add deploys from github to SalesForce
+- Polish architecture of the files
+
+
---
+++ tests/controller_extensions/TestOmniBrowseControllerExtension.java
@@ -0,0 +1,74 @@
+@isTest
+private class TestOmniBrowseControllerExtension {
+
+  static testMethod void testGetExternalIdValueWhenEmail(){
+    final String contactEmail='test@omnibrowse.com';
+    final String contactLastName = 'LastName';
+    
+    User user = new User(
+      ProfileId = [SELECT Id FROM Profile WHERE Name = 'Standard User' LIMIT 1].Id,
+      LastName = 'Last',
+      Email = 'user@omnibrowse.com',
+      Username = 'user@omnibrowse.com' + System.currentTimeMillis(),
+      CompanyName = 'SaleMove',
+      Title = 'title',
+      Alias = 'alias',
+      TimeZoneSidKey = 'America/New_York',
+      EmailEncodingKey = 'UTF-8',
+      LanguageLocaleKey = 'en_US',
+      LocaleSidKey = 'en_US',
+      ExternalIDFieldName__c = 'Email'
+    );
+
+    insert user;
+
+    System.runas(user)
+    {
+      Contact contact = new Contact(Email=contactEmail, LastName=contactLastName);
+      insert contact;
+      Test.startTest();
+      ApexPages.StandardController contactController = new ApexPages.StandardController(contact);
+      OmniBrowseControllerExtension obExtension = new OmniBrowseControllerExtension(contactController);
+      ApexPages.currentPage().getParameters().put('id', contact.id);
+      String externalIdValue = obExtension.getExternalIdValue();
+      System.assertEquals(contactEmail, externalIdValue);
+
+    }
+    Test.stopTest();
+  }
+
+  static testMethod void testGetExternalIdValueWhenNotEmail(){
+    final String contactEmail='test@omnibrowse.com';
+    final String contactLastName = 'LastName';
+    User user = new User(
+      ProfileId = [SELECT Id FROM Profile WHERE Name = 'Standard User' LIMIT 1].Id,
+      LastName = 'Last',
+      Email = 'user@omnibrowse.com',
+      Username = 'user@omnibrowse.com' + System.currentTimeMillis(),
+      CompanyName = 'SaleMove',
+      Title = 'title',
+      Alias = 'alias',
+      TimeZoneSidKey = 'America/New_York',
+      EmailEncodingKey = 'UTF-8',
+      LanguageLocaleKey = 'en_US',
+      LocaleSidKey = 'en_US',
+      ExternalIDFieldName__c = 'LastName'
+    );
+
+    insert user;
+    System.runas(user)
+    {
+      Contact contact = new Contact(Email=contactEmail, LastName=contactLastName);
+      insert contact;
+      Test.startTest();
+      
+      ApexPages.StandardController contactController = new ApexPages.StandardController(contact);
+      OmniBrowseControllerExtension obExtension = new OmniBrowseControllerExtension(contactController);
+      ApexPages.currentPage().getParameters().put('id', contact.id);
+      String externalIdValue = obExtension.getExternalIdValue();
+      System.assertEquals(contactLastName, externalIdValue);
+    }
+    Test.stopTest();
+  }
+
+}
