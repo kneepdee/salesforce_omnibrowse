9b2f6fb02e2a74e5f22b047af7a9f08bf3bcb3b2
paniagua <carlos.paniagua@salemove.com>
2017-06-07 13:00:26 +0300

    Enable integrator to configure Model and Field
    OB-154

--- controllers_extensions/omnibrowse_controller_extension.java
+++ controllers_extensions/omnibrowse_controller_extension.java
@@ -1,15 +1,15 @@
 public class OmniBrowseControllerExtension {
-   
+
   public Contact record { get; private set; }
   private String externalId = '';
   private String externalIdFieldName = 'Email';
   private User currentuser {get; set;}
-  
+
   public OmniBrowseControllerExtension(ApexPages.standardController std)
-  {   
+  {
     currentuser=new User();
     currentuser=[Select ExternalIDFieldName__c from User where Id=:userinfo.getuserId()];
-    externalIdFieldName = string.valueof(currentUser.get('ExternalIDFieldName__c'));        
+    externalIdFieldName = string.valueof(currentUser.get('ExternalIDFieldName__c'));
     if (std.getId() != null) {
       String id = std.getId();
       String query = 'SELECT '+ externalIdFieldName + ' FROM Contact WHERE Id = :id';
@@ -20,7 +20,7 @@ public class OmniBrowseControllerExtension {
       }
     }
   }
-  
+
   public String getExternalIdValue(){
     return externalId;
   }
